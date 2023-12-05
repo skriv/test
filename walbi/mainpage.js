79d2ee846603fe12ab4fc8423e7f189514ad5183
Alex Krivov <skriv80@gmail.com>
2023-12-05 19:33:24 +0600

    Walbi

--- walbi/mainpage.js
+++ walbi/mainpage.js
@@ -1,8 +1,10 @@
-console.log("Hello Walbi");
+console.log("Hello Walbi1111");
 
 // const ANDRIOD_BUTTON_NAME = "Download the app";
 // const ANDROID_LINK = "https://play.google.com/store/apps/details?id=com.walbi.android";
 // const JOIN_LINK = "https://app.walbi.com/"
+
+var EMPTY = "";
 
 
 $(document).ready(function(){
@@ -23,7 +25,26 @@ $(document).ready(function(){
   } else {
     mainButton.text(JOIN_BUTTON_NAME).attr('href', JOIN_LINK);
   }
+
+  $('#blog-list').append('<div id="empty">&nbsp;</div>');
+
+  EMPTY = $('#empty');
+    updatePadding();
 });
+
+
+
+
+// Функция для обновления padding-right у элемента #empty
+function updatePadding() {
+  var windowWidth = $(window).width();
+  var swiperWrapperWidth = $('.swiper-wrapper').width();
+  var difference = (windowWidth - swiperWrapperWidth) / 2;
+  EMPTY.css({
+    'padding-right': difference + 'px',
+    'margin-right': -difference + 'px'
+});
+}
 
 
 
@@ -54,12 +75,15 @@ ppp = ppp - 16;
 let cont = $(".container-large").width();
 
 var endX = cont - result - ppp;
+console.log(endX)
 
 // // создаем твин, который перемещает элемент по горизонтали
 tl.to("#currency", {
   x: endX,
   ease: "none" // отключаем плавность анимации, чтобы получить плавный скроллинг
 });
+
+
 
 // // создаем триггер ScrollTrigger для запуска анимации
 ScrollTrigger.create({
@@ -71,85 +95,15 @@ ScrollTrigger.create({
   //markers: true // показывать маркеры для отладки
 });
 
+
+
 $(window).resize(function () {
   // код, который нужно выполнить при изменении размера окна
   cont = $(".container-large").width();
   endX = cont - result - ppp;
   // console.log("cont");
   ScrollTrigger.update();
-});
 
 
-
-
-
-
-
-// BLOG SWIPER
-
-$(".slider-main_component").each(function (index) {
-  let loopMode = false;
-  if ($(this).attr("loop-mode") === "true") {
-    loopMode = true;
-  }
-  let sliderDuration = 300;
-  if ($(this).attr("slider-duration") !== undefined) {
-    sliderDuration = +$(this).attr("slider-duration");
-  }
-  const swiper = new Swiper($(this).find(".swiper")[0], {
-    speed: sliderDuration,
-    loop: loopMode,
-    autoHeight: false,
-    centeredSlides: loopMode,
-    followFinger: true,
-    freeMode: false,
-    slideToClickedSlide: false,
-    slidesPerView: 1,
-    spaceBetween: "4%",
-    rewind: false,
-    mousewheel: {
-      forceToAxis: true
-    },
-    keyboard: {
-      enabled: true,
-      onlyInViewport: true
-    },
-    breakpoints: {
-      // mobile landscape
-      480: {
-        slidesPerView: 300,
-        spaceBetween: 8
-      },
-      // tablet
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 8
-      },
-      // desktop
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 8
-      }
-    },
-    pagination: {
-      el: $(this).find(".swiper-bullet-wrapper")[0],
-      bulletActiveClass: "is-active",
-      bulletClass: "swiper-bullet",
-      bulletElement: "button",
-      clickable: true
-    },
-    navigation: {
-      nextEl: $(this).find(".swiper-next")[0],
-      prevEl: $(this).find(".swiper-prev")[0],
-      disabledClass: "is-disabled"
-    },
-    scrollbar: {
-      el: $(this).find(".swiper-drag-wrapper")[0],
-      draggable: true,
-      dragClass: "swiper-drag",
-      snapOnRelease: true
-    },
-    slideActiveClass: "is-active",
-    slideDuplicateActiveClass: "is-active"
-  });
+  updatePadding();
 });
