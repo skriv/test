1164b4ddf80abac602d41a7bd365cbcfcb10643e
Alex Krivov <skriv80@gmail.com>
2024-02-16 13:35:24 +0600

    Add Walbi Code

---
+++ walbi/buttons.js
@@ -0,0 +1,105 @@
+console.log("UTM");
+
+var EMPTY = "";
+
+var IOS_BUTTON_NAME = "Download iOS Beta";
+var IOS_LINK = "https://testflight.apple.com/join/QRbXi8u3";
+
+var IOS_INDIA = "Download";
+var IOS_INDIA_LINK =
+  "https://apps.apple.com/us/app/walbi-ai-crypto-assistant/id6451312745";
+
+var ANDROID_BUTTON_NAME = "Download";
+var ANDROID_LINK =
+  "https://play.google.com/store/apps/details?id=com.walbi.android";
+
+var JOIN_LINK = "https://app.walbi.com/";
+var JOIN_BUTTON_NAME = "Join";
+
+var HEADER_BUTTON = "Open App";
+
+// USER AGENT
+var userAgent = navigator.userAgent.toLowerCase();
+var isAndroid = userAgent.indexOf("android") > -1;
+var isIOS = /iphone|ipad|ipod/.test(userAgent); // Проверяем наличие iOS устройств
+
+var mainButton = $("button[data-utm], a[data-utm]");
+var userCountry = null;
+
+$(document).ready(function () {
+  initializeMainButton();
+});
+
+//GEOLOCATION
+
+function initializeMainButton() {
+  if (isAndroid) {
+    setMainButton(ANDROID_BUTTON_NAME, ANDROID_LINK);
+  } else if (isIOS) {
+    setupIosButton();
+  } else {
+    setMainButton(JOIN_BUTTON_NAME, JOIN_LINK);
+  }
+}
+
+function setMainButton(text, href) {
+  mainButton.each(function() {
+    // Проверяем наличие атрибута у каждой кнопки
+    if (!$(this).attr('data-open-app')) {
+      // Если атрибута нет, обновляем текст и href
+      $(this).text(text).attr("href", href);
+    }
+  });
+  
+  // Добавляем UTM-параметры к нужным кнопкам
+  setUTMtoButtons();
+}
+
+function setupIosButton() {
+  if (userCountry === null) {
+    $.getJSON("https://ipinfo.io", function (data) {
+      if (data && data.country) {
+        userCountry = data.country;
+        updateIosButton();
+      }
+    }).fail(function () {
+      console.error("Error request to ipinfo.io");
+    });
+  } else {
+    updateIosButton();
+  }
+}
+
+function updateIosButton() {
+  if (userCountry === "IN") {
+    setMainButton(IOS_INDIA, IOS_INDIA_LINK);
+  } else {
+    setMainButton(IOS_BUTTON_NAME, IOS_LINK);
+  }
+}
+
+//UTM's
+
+function setUTMtoButtons() {
+  // Выбираем все ссылки с атрибутом data-utm
+  mainButton.each(function () {
+    var currentUrl = new URL(window.location.href);
+    var linkUrl = new URL($(this).attr("href"));
+
+    // Перечисляем UTM-параметры, которые нужно добавить или обновить
+    [
+      "utm_campaign",
+      "utm_source",
+      "utm_medium",
+      "utm_term",
+      "utm_content",
+    ].forEach(function (param) {
+      let value = currentUrl.searchParams.get(param);
+      // Добавляем параметр, только если он есть в текущем URL
+      if (value) linkUrl.searchParams.set(param, value);
+    });
+
+    // Обновляем href ссылки
+    $(this).attr("href", linkUrl.toString());
+  });
+}
--- walbi/mainpage.js
+++ walbi/mainpage.js
@@ -1,101 +1,29 @@
-console.log("Hello WalbiProduction");
+console.log("Hello Main Page");
 
 var EMPTY = "";
 
-var IOS_BUTTON_NAME = "Download iOS Beta";
-var IOS_LINK = "https://testflight.apple.com/join/QRbXi8u3"
-
-var IOS_INDIA = "Download";
-var IOS_INDIA_LINK = "https://apps.apple.com/us/app/walbi-ai-crypto-assistant/id6451312745";
-
-var ANDROID_BUTTON_NAME = "Download";
-var ANDROID_LINK = "https://play.google.com/store/apps/details?id=com.walbi.android";
-
-var JOIN_LINK = "https://app.walbi.com/";
-var JOIN_BUTTON_NAME = "Join";
-
-
-// USER AGENT
-var userAgent = navigator.userAgent.toLowerCase();
-var isAndroid = userAgent.indexOf("android") > -1; 
-var isIOS = /iphone|ipad|ipod/.test(userAgent); // Проверяем наличие iOS устройств
-
-
-var mainButton = $('.hero-section');
-var userCountry = null;
-
-$(document).ready(function() {
-  initializeMainButton();
+$(document).ready(function () {
   initializeBlogList();
 });
 
-function initializeMainButton() {
-  if (isAndroid) {
-    setMainButton(ANDROID_BUTTON_NAME, ANDROID_LINK);
-  } else if (isIOS) {
-    setupIosButton();
-  } else {
-    setMainButton(JOIN_BUTTON_NAME, JOIN_LINK);
-  }
-}
-
-function setMainButton(text, href) {
-  mainButton.text(text).attr('href', href);
-  console.log(href);
-}
-
-function setupIosButton() {
-  if (userCountry === null) {
-    $.getJSON('https://ipinfo.io', function(data) {
-      if (data && data.country) {
-        userCountry = data.country;
-        updateIosButton();
-      }
-    }).fail(function() {
-      console.error('Error request to ipinfo.io');
-    });
-  } else {
-    updateIosButton();
-  }
-}
-
-function updateIosButton() {
-  if (userCountry === 'IN') {
-    setMainButton(IOS_INDIA, IOS_INDIA_LINK);
-  } else {
-    setMainButton(IOS_BUTTON_NAME, IOS_LINK);
-  }
-}
 
 function initializeBlogList() {
-  $('#blog-list').append('<div id="empty">&nbsp;</div>');
-  EMPTY = $('#empty');
+  $("#blog-list").append('<div id="empty">&nbsp;</div>');
+  EMPTY = $("#empty");
   resetAndRecalculate();
 }
 
-
-
 // Add Padding to Blog Section
 function resetAndRecalculate() {
-  var swiperWrapper = $('.container-large');
-
-  // Сброс положения элемента, если нужно
-  // Например, 
-  // swiperWrapper.css('left', '0px');
+  var swiperWrapper = $(".container-large")
 
   // Вычисление и вывод новой позиции
   var distance = swiperWrapper.offset().left;
   EMPTY.css({
-    'padding-right': distance + 'px',
-    'margin-right': -distance + 'px'    
+    "padding-right": distance + "px",
+    "margin-right": -distance + "px",
   });
 }
-
-
-
-
-
-
 
 // //GSAP
 gsap.registerPlugin(ScrollTrigger);
@@ -106,8 +34,8 @@ var ScrollTrigger = window.ScrollTrigger;
 // // создаем анимацию с помощью GSAP
 var tl = gsap.timeline({
   defaults: {
-    duration: 1 // длительность анимации
-  }
+    duration: 1, // длительность анимации
+  },
 });
 
 const containers = $(".test-scroll");
@@ -127,10 +55,8 @@ var endX = cont - result - ppp;
 // // создаем твин, который перемещает элемент по горизонтали
 tl.to("#currency", {
   x: endX,
-  ease: "none" // отключаем плавность анимации, чтобы получить плавный скроллинг
+  ease: "none", // отключаем плавность анимации, чтобы получить плавный скроллинг
 });
-
-
 
 // создаем триггер ScrollTrigger для запуска анимации
 ScrollTrigger.create({
@@ -138,10 +64,9 @@ ScrollTrigger.create({
   start: "top 95%", // точка начала анимации (верх центра экрана)
   end: "bottom 5%", // точка окончания анимации (низ центра экрана
   animation: tl, // анимация, которую мы создали с помощью GSAP
-  scrub: true // плавность анимации при скроллинге
+  scrub: true, // плавность анимации при скроллинге
   //markers: true // показывать маркеры для отладки
 });
-
 
 // код, который нужно выполнить при изменении размера окна
 $(window).resize(function () {
@@ -151,5 +76,3 @@ $(window).resize(function () {
 
   resetAndRecalculate(); // Add Padding to Blog Section
 });
-
-
