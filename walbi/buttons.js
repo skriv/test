d431dbe0852d634562476c167c9e8c2a9aee8783
Alex Krivov <skriv80@gmail.com>
2024-02-16 19:48:45 +0600

    Walbi

--- walbi/buttons.js
+++ walbi/buttons.js
@@ -1,30 +1,30 @@
-console.log("Hello Main Page");
+console.log("Hello Buttons");
 
-var EMPTY = "";
 
 var IOS_BUTTON_NAME = "Download iOS Beta";
-var IOS_LINK = "https://testflight.apple.com/join/QRbXi8u3";
+var IOS_LINK = "https://testflight.apple.com/join/QRbXi8u3"
 
 var IOS_INDIA = "Download";
-var IOS_INDIA_LINK =
-  "https://apps.apple.com/us/app/walbi-ai-crypto-assistant/id6451312745";
+var IOS_INDIA_LINK = "https://apps.apple.com/us/app/walbi-ai-crypto-assistant/id6451312745";
 
 var ANDROID_BUTTON_NAME = "Download";
-var ANDROID_LINK =
-  "https://play.google.com/store/apps/details?id=com.walbi.android";
+var ANDROID_LINK = "https://play.google.com/store/apps/details?id=com.walbi.android";
 
 var JOIN_LINK = "https://app.walbi.com/";
 var JOIN_BUTTON_NAME = "Join";
 
+var REGION = 'IN';
+
 // USER AGENT
 var userAgent = navigator.userAgent.toLowerCase();
-var isAndroid = userAgent.indexOf("android") > -1;
+var isAndroid = userAgent.indexOf("android") > -1; 
 var isIOS = /iphone|ipad|ipod/.test(userAgent); // Проверяем наличие iOS устройств
 
-var mainButton = $(".hero-section");
+
+var mainButton = $("button[data-utm], a[data-utm]");
 var userCountry = null;
 
-$(document).ready(function () {
+$(document).ready(function() {
   initializeMainButton();
 });
 
@@ -39,19 +39,30 @@ function initializeMainButton() {
 }
 
 function setMainButton(text, href) {
-  mainButton.text(text).attr("href", href);
+
+  mainButton.each(function() {
+    // Проверяем, имеет ли элемент атрибут data-nochange. Если атрибут отсутствует, attr() вернет undefined
+    if ($(this).attr('data-open-app') === "open-app") {
+      $(this).attr("href");
+    }else{
+      $(this).text(text).attr('href', href);
+    }
+  });
+
+  // mainButton.text(text).attr('href', href);
   console.log(href);
+  setUTMtoButtons();
 }
 
 function setupIosButton() {
   if (userCountry === null) {
-    $.getJSON("https://ipinfo.io", function (data) {
+    $.getJSON('https://ipinfo.io', function(data) {
       if (data && data.country) {
         userCountry = data.country;
         updateIosButton();
       }
-    }).fail(function () {
-      console.error("Error request to ipinfo.io");
+    }).fail(function() {
+      console.error('Error request to ipinfo.io');
     });
   } else {
     updateIosButton();
@@ -59,9 +70,45 @@ function setupIosButton() {
 }
 
 function updateIosButton() {
-  if (userCountry === "IN") {
+  if (userCountry === REGION) {
     setMainButton(IOS_INDIA, IOS_INDIA_LINK);
   } else {
     setMainButton(IOS_BUTTON_NAME, IOS_LINK);
   }
 }
+
+
+function setUTMtoButtons() {
+  // Выбираем все ссылки с атрибутом data-utm
+  mainButton.each(function () {
+    var currentUrl = new URL(window.location.href);
+    var linkUrl = new URL($(this).attr("href"));
+
+    // Перечисляем UTM-параметры, которые нужно добавить или обновить
+    [
+      "utm_campaign",
+      "utm_source",
+      "utm_medium",
+      "utm_term",
+      "utm_content",
+    ].forEach(function (param) {
+      let value = currentUrl.searchParams.get(param);
+      // Добавляем параметр, только если он есть в текущем URL
+      if (value) linkUrl.searchParams.set(param, value);
+    });
+
+    // Обновляем href ссылки
+    $(this).attr("href", linkUrl.toString());
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
--- walbi/mainpage.js
+++ walbi/mainpage.js
@@ -1,71 +1,13 @@
-console.log("Hello WalbiProduction");
+console.log("Hello MainPage");
 
 var EMPTY = "";
 
-var IOS_BUTTON_NAME = "Download iOS Beta";
-var IOS_LINK = "https://testflight.apple.com/join/QRbXi8u3"
-
-var IOS_INDIA = "Download";
-var IOS_INDIA_LINK = "https://apps.apple.com/us/app/walbi-ai-crypto-assistant/id6451312745";
-
-var ANDROID_BUTTON_NAME = "Download";
-var ANDROID_LINK = "https://play.google.com/store/apps/details?id=com.walbi.android";
-
-var JOIN_LINK = "https://app.walbi.com/";
-var JOIN_BUTTON_NAME = "Join";
-
-
-// USER AGENT
-var userAgent = navigator.userAgent.toLowerCase();
-var isAndroid = userAgent.indexOf("android") > -1; 
-var isIOS = /iphone|ipad|ipod/.test(userAgent); // Проверяем наличие iOS устройств
-
-
-var mainButton = $('.hero-section');
-var userCountry = null;
 
 $(document).ready(function() {
-  initializeMainButton();
   initializeBlogList();
 });
 
-function initializeMainButton() {
-  if (isAndroid) {
-    setMainButton(ANDROID_BUTTON_NAME, ANDROID_LINK);
-  } else if (isIOS) {
-    setupIosButton();
-  } else {
-    setMainButton(JOIN_BUTTON_NAME, JOIN_LINK);
-  }
-}
 
-function setMainButton(text, href) {
-  mainButton.text(text).attr('href', href);
-  console.log(href);
-}
-
-function setupIosButton() {
-  if (userCountry === null) {
-    $.getJSON('https://ipinfo.io', function(data) {
-      if (data && data.country) {
-        userCountry = data.country;
-        updateIosButton();
-      }
-    }).fail(function() {
-      console.error('Error request to ipinfo.io');
-    });
-  } else {
-    updateIosButton();
-  }
-}
-
-function updateIosButton() {
-  if (userCountry === 'IN') {
-    setMainButton(IOS_INDIA, IOS_INDIA_LINK);
-  } else {
-    setMainButton(IOS_BUTTON_NAME, IOS_LINK);
-  }
-}
 
 function initializeBlogList() {
   $('#blog-list').append('<div id="empty">&nbsp;</div>');
@@ -90,10 +32,6 @@ function resetAndRecalculate() {
     'margin-right': -distance + 'px'    
   });
 }
-
-
-
-
 
 
 
